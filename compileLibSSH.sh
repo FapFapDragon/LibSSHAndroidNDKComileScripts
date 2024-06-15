019a23ffb5d94be222dccf49fa59faacf1cc499f
Troy Walther <48346054+FapFapDragon@users.noreply.github.com>
2024-06-15 13:37:23 -0400

    Added Scripts

---
+++ compileLibSSH.sh
@@ -0,0 +1,90 @@
+ANDROID_API="arm64-v8a"
+API_LEVEL=27
+BUILD_SHARED_LIB=0
+SCRIPTPATH=/home/vm/Downloads/LibSSH
+export ANDROID_NDK_ROOT=$SCRIPTPATH/android-ndk-r26d
+NDK_TOOLCHAIN_PATH=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake
+architecture=android-arm64
+OUTPUT_LIBS_DIR="libs/${ANDROID_API}"
+build_static_libssh=ON
+
+rm -rf libssh_source
+git clone https://git.libssh.org/projects/libssh.git libssh_source
+
+#PATCH to fix Compilation with Android NDK
+sed -i 's/_S_IWRITE/S_IWUSR/g' libssh_source/src/misc.c
+sed -i 's/S_IWRITE/S_IWUSR/g' libssh_source/src/misc.c
+
+rm -rf libssh_$ANDROID_API
+cp -R libssh_source libssh_$ANDROID_API
+cd libssh_$ANDROID_API
+mkdir build
+cd build
+
+make clean
+rm -rf 
+
+cmake \
+	-DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/libssh_$architecture \
+	-DWITH_INTERNAL_DOC=OFF \
+	-DWITH_GSSAPI=OFF \
+	-DWITH_NACL=OFF \
+	-DWITH_EXAMPLES=OFF \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DOPENSSL_INCLUDE_DIR=$SCRIPTPATH/openssl_$architecture/include \
+	-DOPENSSL_CRYPTO_LIBRARY=$SCRIPTPATH/openssl_$architecture/lib/libcrypto.a \
+	-DCMAKE_TOOLCHAIN_FILE=${NDK_TOOLCHAIN_PATH} \
+	-DANDROID_NDK=${ANDROID_NDK_ROOT} \
+	-DANDROID_NATIVE_API_LEVEL=android-${API_LEVEL} \
+	-DANDROID_ABI=${ANDROID_API} \
+	-DBUILD_STATIC_LIB=${build_static_libssh} \
+	-DBUILT_LIBS_DIR=${OUTPUT_LIBS_DIR} \
+	-DWITH_SFTP=1 \
+	-DCLIENT_TESTING=1\
+	..
+cmake --build 
+make install
+cp src/libssh.a $SCRIPTPATH/libssh_$architecture/lib/
+cd $SCRIPTPATH
+rm -rf libssh_$ANDROID_API
+	
+ANDROID_API="armeabi-v7a"
+API_LEVEL=27
+BUILD_SHARED_LIB=0
+SCRIPTPATH=/home/vm/Downloads/LibSSH
+export ANDROID_NDK_ROOT=$SCRIPTPATH/android-ndk-r26d
+NDK_TOOLCHAIN_PATH=$ANDROID_NDK_ROOT/build/cmake/android.toolchain.cmake
+architecture=android-arm
+OUTPUT_LIBS_DIR="libs/${ANDROID_API}"
+cd $SCRIPTPATH
+rm -rf libssh_$ANDROID_API
+cp -R libssh_source libssh_$ANDROID_API
+cd libssh_$ANDROID_API
+mkdir build
+cd build
+
+cmake \
+	-DCMAKE_INSTALL_PREFIX=$SCRIPTPATH/libssh_$architecture \
+	-DWITH_INTERNAL_DOC=OFF \
+	-DWITH_GSSAPI=OFF \
+	-DWITH_NACL=OFF \
+	-DWITH_EXAMPLES=OFF \
+	-DCMAKE_BUILD_TYPE=Release \
+	-DOPENSSL_INCLUDE_DIR=$SCRIPTPATH/openssl_$architecture/include \
+	-DOPENSSL_CRYPTO_LIBRARY=$SCRIPTPATH/openssl_$architecture/lib/libcrypto.a \
+	-DCMAKE_TOOLCHAIN_FILE=${NDK_TOOLCHAIN_PATH} \
+	-DANDROID_NDK=${ANDROID_NDK_ROOT} \
+	-DANDROID_NATIVE_API_LEVEL=android-${API_LEVEL} \
+	-DANDROID_ABI=${ANDROID_API} \
+	-DBUILD_STATIC_LIB=${build_static_libssh} \
+	-DBUILT_LIBS_DIR=${OUTPUT_LIBS_DIR} \
+	-DWITH_SFTP=1 \
+	-DCLIENT_TESTING=1\
+	..
+	
+cmake --build 
+make install
+cp src/libssh.a $SCRIPTPATH/libssh_$architecture/lib/
+cd $SCRIPTPATH
+rm -rf libssh_$ANDROID_API
+
---
+++ compileOpenSSL.sh
@@ -0,0 +1,45 @@
+SCRIPTPATH=/home/vm/Downloads/LibSSH
+export ANDROID_NDK_HOME=$SCRIPTPATH/android-ndk-r26d
+export ANDROID_NDK_ROOT=$SCRIPTPATH/android-ndk-r26d
+OPENSSL_DIR=$SCRIPTPATH/openssl-master
+toolchains_path=$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64
+
+export PATH="$ANDROID_NDK_HOME/toolchains/llvm/prebuilt/linux-x86_64/bin:$PATH"
+
+CC=clang
+
+# Add toolchains bin directory to PATH
+PATH=$toolchains_path/bin:$PATH
+
+# Set the Android API levels
+ANDROID_API=27
+
+# Set the target architecture
+# Can be android-arm, android-arm64, android-x86, android-x86 etc
+architecture=android-arm
+
+git clone https://github.com/openssl/openssl.git $OPENSSL_DIR
+ 
+cd $OPENSSL_DIR
+
+./Configure $architecture -D__ANDROID_MIN_SDK_VERSION__=$ANDROID_API -fPIC --prefix=$SCRIPTPATH/openssl_$architecture --openssldir=$SCRIPTPATH/openssl_$architecture/openssl_dir 
+
+make clean 
+
+sed -e '/[.]hidden.*OPENSSL_armcap_P/d; /[.]extern.*OPENSSL_armcap_P/ {p; s/extern/hidden/ }' -i -- $OPENSSL_DIR/crypto/*arm*pl $OPENSSL_DIR/crypto/*/asm/*arm*pl
+
+make 
+
+make install
+
+architecture=android-arm64
+./Configure $architecture -D__ANDROID_MIN_SDK_VERSION__=$ANDROID_API --prefix=$SCRIPTPATH/openssl_$architecture --openssldir=$SCRIPTPATH/openssl_$architecture/openssl_dir
+
+make clean 
+
+make 
+
+make install
+
+echo $PATH
+
